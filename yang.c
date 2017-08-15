0dee854c4b9504b8efc528878b8e1c38b755059a
jiangxiujie <jiangxiujie@cantv.cn>
2017-08-15 16:56:05 +0800

    h2板子红灯闪

--- yang.c
+++ yang.c
@@ -16,38 +16,38 @@
 #include <linux/kthread.h>
 #include <linux/earlysuspend.h>
 
-#define lcd_backlight_msg(...)    do {printk("[lcd_backlight]: "__VA_ARGS__);} while(0)
+#define led_backlight_msg(...)    do {printk("[led_backlight]: "__VA_ARGS__);} while(0)
 
-static struct _lcd_backlight_info {
+static struct _led_backlight_info {
 	int backlight_used;
 	int backlight_on;
 	struct task_struct *tsk;
 #ifdef CONFIG_HAS_EARLYSUSPEND
 	struct early_suspend early_suspend;
 #endif
-}lcd_backlight_info;
+}led_backlight_info;
 
-static char *lcd_backlight = "lcd_backlight";
+static char *led_backlight = "led_backlight";
 
-static struct platform_device lcd_backlight_dev = {
-	.name           = "lcd_backlight",
+static struct platform_device led_backlight_dev = {
+	.name           = "led_backlight",
 };
 
 #ifdef CONFIG_PM
-static int lcd_backlight_suspend(struct device *dev)
+static int led_backlight_suspend(struct device *dev)
 {
-	printk("lcd_backlight_suspend---------------------\n");
+	printk("led_backlight_suspend---------------------\n");
 	return 0;
 }
 
-static int lcd_backlight_resume(struct device *dev)
+static int led_backlight_resume(struct device *dev)
 {
 	return 0;
 }
 
-static struct dev_pm_ops lcd_backlight_dev_pm_ops = {
-	.suspend    = lcd_backlight_suspend,
-	.resume     = lcd_backlight_resume,
+static struct dev_pm_ops led_backlight_dev_pm_ops = {
+	.suspend    = led_backlight_suspend,
+	.resume     = led_backlight_resume,
 };
 #endif
 
@@ -91,31 +91,31 @@ int sunxi_gpio_req(struct gpio_config *gpio)
 	return 0;
 }
 
-static int lcd_backlight_get_res(void)
+static int led_backlight_get_res(void)
 {
 	//获取的gpio是数字 ?
 	script_item_value_type_e type;
 	script_item_u val; 
 	struct gpio_config  *gpio_p = NULL;
 
-	type = script_get_item(lcd_backlight, "backlight_used", &val);
+	type = script_get_item(led_backlight, "backlight_used", &val);
 	if (SCIRPT_ITEM_VALUE_TYPE_INT != type) {
-		lcd_backlight_msg("------------failed to fetch lcd backlight configuration!\n");
+		led_backlight_msg("------------failed to fetch lcd backlight configuration!\n");
 		return -1;
 	}
 	if (!val.val) {
-		lcd_backlight_msg("-----------no lcd backlight used in configuration\n");
+		led_backlight_msg("-----------no lcd backlight used in configuration\n");
 		return -1;
 	}
-	lcd_backlight_info.backlight_used = val.val;
+	led_backlight_info.backlight_used = val.val;
 
-	lcd_backlight_info.backlight_on = -1;
-	type = script_get_item(lcd_backlight, "backlight_on", &val);
+	led_backlight_info.backlight_on = -1;
+	type = script_get_item(led_backlight, "backlight_on", &val);
 	if (SCIRPT_ITEM_VALUE_TYPE_PIO!=type)
-		lcd_backlight_msg("-----------------get lcd backlight_on gpio failed\n");
+		led_backlight_msg("-----------------get lcd backlight_on gpio failed\n");
 	else {
 		gpio_p = &val.gpio;
-		lcd_backlight_info.backlight_on = gpio_p->gpio;
+		led_backlight_info.backlight_on = gpio_p->gpio;
 		sunxi_gpio_req(gpio_p);
 	}
 
@@ -131,21 +131,41 @@ static int flick_func(void *noused)
 		msleep(500);
 		msleep(500);
 		printk("--------------set 1------\n");
-		gpio_direction_output(lcd_backlight_info.backlight_on, 1);
-		gpio_set_value(lcd_backlight_info.backlight_on, 1);
+		gpio_direction_output(led_backlight_info.backlight_on, 1);
+		gpio_set_value(led_backlight_info.backlight_on, 1);
 		msleep(500);
 		msleep(500);
 		printk("--------------set 0------\n");
-		gpio_set_value(lcd_backlight_info.backlight_on, 0);
+		gpio_set_value(led_backlight_info.backlight_on, 0);
 	}
-	gpio_free(lcd_backlight_info.backlight_on);
+	gpio_free(led_backlight_info.backlight_on);
 
 	return 0;
 }
 
-static void lcd_backlight_set_flick(void) {
-	struct task_struct *ptsk = lcd_backlight_info.tsk;
-	ptsk = kthread_run(flick_func, NULL, "lcd flick");
+static int flick_func_raw(void *noused)
+{
+	//gpio: what request dir set free
+	//PL10 
+	int gpio_num = ('L' - 'A') * 32 + 10;
+	printk("gpio_num: %d\n", gpio_num);
+
+	gpio_request(gpio_num, "pwr");
+	while (1) {
+		gpio_direction_output(gpio_num, 1);
+		gpio_set_value(gpio_num, 1);
+		msleep(500);
+		msleep(500);
+		gpio_set_value(gpio_num, 0);
+		msleep(500);
+		msleep(500);
+	}
+	return 0;
+}
+
+static void led_backlight_set_flick(void) {
+	struct task_struct *ptsk = led_backlight_info.tsk;
+	ptsk = kthread_run(flick_func_raw, NULL, "lcd flick");
 	if (IS_ERR(ptsk)) {
 		printk(KERN_INFO "-----------create kthread failed!\n");
 	}
@@ -154,64 +174,64 @@ static void lcd_backlight_set_flick(void) {
 #ifdef CONFIG_HAS_EARLYSUSPEND
 static void lcd_early_suspend(struct early_suspend *h)
 {
-	struct _lcd_backlight_info *lcd;
-	lcd = container_of(h, struct _lcd_backlight_info, early_suspend);
+	struct _led_backlight_info *lcd;
+	lcd = container_of(h, struct _led_backlight_info, early_suspend);
 	printk("------early suspend-----\n");
 //do nothing, put it to Runtime
 	
 }
 #endif
 
-static int __devinit lcd_backlight_probe(struct platform_device *pdev) 
+static int __devinit led_backlight_probe(struct platform_device *pdev) 
 {
 	//0.配置fex方式:       方便查看类似板级文件的使用情况，需要驱动中对此fex配置进行解析。
 	//  另外就是直接方式： 可以参考本目录下的test文件（AllWinner提供）是(bank - 1) *32 + offset来做：标准linux kernel形式
-	lcd_backlight_msg("--------------lcd_backlight dev probe\n");
+	led_backlight_msg("--------------led_backlight dev probe\n");
 	//1.解析平台fex配置,获取资源
-	lcd_backlight_get_res();	
+	led_backlight_get_res();	
 
 	//2.创建线程闪屏30次，每次500ms
-	lcd_backlight_set_flick();
+	led_backlight_set_flick();
 	
 	//0.注册early suspend
 #ifdef CONFIG_HAS_EARLYSUSPEND
-	lcd_backlight_info.early_suspend.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN + 1;
-	lcd_backlight_info.early_suspend.suspend = lcd_early_suspend;
+	led_backlight_info.early_suspend.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN + 1;
+	led_backlight_info.early_suspend.suspend = lcd_early_suspend;
 	printk("---------register early suspend---\n");
-	register_early_suspend(&lcd_backlight_info.early_suspend);
+	register_early_suspend(&led_backlight_info.early_suspend);
 #endif
 	return 0;
 }
 
-static int __devexit lcd_backlight_remove(struct platform_device *pdev)
+static int __devexit led_backlight_remove(struct platform_device *pdev)
 {
-	lcd_backlight_msg("lcd_backlight dev remove\n");
+	led_backlight_msg("led_backlight dev remove\n");
 
 	return 0;
 } 
 
-static struct platform_driver lcd_backlight_driver = {
+static struct platform_driver led_backlight_driver = {
 	.driver = {
-		.name  = "lcd_backlight",
+		.name  = "led_backlight",
 		.owner = THIS_MODULE,
 #ifdef CONFIG_PM
-		.pm    = &lcd_backlight_dev_pm_ops,
+		.pm    = &led_backlight_dev_pm_ops,
 #endif
 	},
-	.probe          = lcd_backlight_probe,
-	.remove         = __devexit_p(lcd_backlight_remove),
+	.probe          = led_backlight_probe,
+	.remove         = __devexit_p(led_backlight_remove),
 };
 
-static int __init lcd_backlight_init(void)
+static int __init led_backlight_init(void)
 {
 	printk("----------init-----------\n");
-	platform_device_register(&lcd_backlight_dev);
-	return platform_driver_register(&lcd_backlight_driver);
+	platform_device_register(&led_backlight_dev);
+	return platform_driver_register(&led_backlight_driver);
 }
 
-static void __exit lcd_backlight_exit(void)
+static void __exit led_backlight_exit(void)
 {
-	struct task_struct *ptsk = lcd_backlight_info.tsk;
+	struct task_struct *ptsk = led_backlight_info.tsk;
 	printk("----------exit-----------\n");
 #if 0
 	if (!IS_ERR(ptsk)){  
@@ -219,11 +239,11 @@ static void __exit lcd_backlight_exit(void)
 		printk(KERN_INFO "------thread function has stop %ds\n", ret);  
 	}
 #endif
-	platform_driver_unregister(&lcd_backlight_driver);
-	platform_device_unregister(&lcd_backlight_dev);
+	platform_driver_unregister(&led_backlight_driver);
+	platform_device_unregister(&led_backlight_dev);
 }
 
-module_init(lcd_backlight_init);
-module_exit(lcd_backlight_exit);
+module_init(led_backlight_init);
+module_exit(led_backlight_exit);
 MODULE_AUTHOR("can jiang");
 MODULE_LICENSE("GPL"); 
